f51fb594e76657c9b6244492b88fd26bd3d14da2
Ashutosh Maurya <92643685+ashu1230@users.noreply.github.com>
2022-11-29 18:26:37 +0530

    Create Array.c

---
+++ DAA/Array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<conio.h> 
+int main()
+{
+  int Array[50], i, N;
+ 
+  printf("\nPlease Enter Number of elements in an array:- ");
+  scanf("%d", &N);
+ 
+  printf("\nPlease Enter %d elements of an Array \n", N);
+  for (i = 0; i < N; i++)
+   {
+     scanf("%d", &Array[i]);
+   }     
+ 
+  printf("\n Elemenst in this Array are :\n");
+  for (i = 0; i < N; i++)
+  {
+ 	printf("%d\t", Array[i]);
+  }     
+ 
+  getch();
+}
