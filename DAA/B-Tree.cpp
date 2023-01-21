3160c0d74f49c6038669522d06e61bb1491d37fe
Ashutosh Maurya <92643685+ashu1230@users.noreply.github.com>
2023-01-21 10:28:59 +0530

    Update B-Tree.cpp

--- DAA/B-Tree.cpp
+++ DAA/B-Tree.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 class BTreeNode
 {
-	int *keys; // An array of keys
+	int *keys;             // An array of keys
 	int t;	 // Minimum degree (defines the range for number of keys)
 	BTreeNode **C; // An array of child pointers
 	int n;	 // Current number of keys
